5f08baf29381af7edf8a206f5deeb0ab55dc9101
Zaid Kajee <zaid26@gmail.com>
2019-10-26 11:12:04 +0200

    Styles in Progress

--- CsvToXlsx/Program.cs
+++ CsvToXlsx/Program.cs
@@ -4,12 +4,14 @@ using OfficeOpenXml.Style;
 using System;
 using System.Globalization;
 using System.IO;
+using System.Linq;
 using System.Text.RegularExpressions;
 
 namespace CsvToXlsx
 {
     class Program
     {
+        private static bool autofit = true;
         static void Main(string[] args)
         {
             string outputFile = args[args.Length - 1];
@@ -20,26 +22,52 @@ namespace CsvToXlsx
                 foreach(string csvFileInfo in args)
                 {
                     string[] fileInfo = csvFileInfo.Split('>');
-                    CreateSheet(fileInfo[0],fileInfo[1], package);
+                    CreateSheet(fileInfo[0],fileInfo[1],fileInfo[2],package);
                 }                
 
-                FileInfo file = new FileInfo(@outputFile);
+                FileInfo file = new FileInfo(outputFile);
                 
                 package.SaveAs(file);
 
             }
         }
 
-        private static void CreateSheet(string csvFileLink,string sheetName, ExcelPackage package)
+        /// <summary>
+        /// 
+        /// Create Sheet
+        /// 
+        /// </summary>
+        /// <param name="csvFileLink"></param>
+        /// <param name="sheetName"></param>
+        /// <param name="package"></param>
+        private static void CreateSheet(string csvFileLink,string sheetName,string dateFormat, ExcelPackage package)
         {
             CultureInfo provider = CultureInfo.InvariantCulture;
 
             // Add a new worksheet to the empty workbook
             ExcelWorksheet worksheet = package.Workbook.Worksheets.Add(sheetName);
             //Add the headers
+           
 
+            CsvFileParser(csvFileLink, dateFormat, provider, worksheet);
+            if (autofit)
+            {
+                worksheet.Cells.AutoFitColumns();
+            }
+            
+        }
 
-            using (TextFieldParser parser = new TextFieldParser(@csvFileLink))
+        /// <summary>
+        /// 
+        /// Parse file and add data to sheet
+        /// 
+        /// </summary>
+        /// <param name="csvFileLink"></param>
+        /// <param name="provider"></param>
+        /// <param name="worksheet"></param>
+        private static void CsvFileParser(string csvFileLink,string dateFormat, CultureInfo provider, ExcelWorksheet worksheet)
+        {
+            using (TextFieldParser parser = new TextFieldParser(csvFileLink))
             {
                 parser.TextFieldType = FieldType.Delimited;
                 parser.SetDelimiters(",");
@@ -58,61 +86,149 @@ namespace CsvToXlsx
                     else
                     {
                         row = Int32.Parse(parser.LineNumber.ToString()) - 1;
-                        
                     }
-                        
+
+                    //Add data to sheet
                     foreach (string field in fields)
                     {
-
-
-
-                        //format Currency
-                        if (Regex.Match(field, @"[0-9]+\.[0-9][0-9]$").Success)
+                        //check if has styling
+                        if (field.Contains("#$#"))
                         {
-                            worksheet.Cells[row, cell].Style.Numberformat.Format = "0.00";
-                            worksheet.Cells[row, cell].Value = double.Parse(field);
+                            string[] fieldData = Regex.Split(field,@"#\$#"); 
+                            
+                            FormatAndInsertValue(provider, worksheet, row, cell, fieldData[0], dateFormat);
+                            
+                            StyleCell(worksheet, row, cell, fieldData.Where((v, i) => i != 0).ToArray());
                         }
-                        //format Float Number
-                        else if (Regex.Match(field, @"[0-9]+\.[0-9][0-9][0-9]+$").Success)
-                        {
-                            string[] parts = field.Split('.');
-                            string points =  "";
-                            for (int i = 0; i < parts[1].Length; i++)
-                            {
-                                points += "0";
-                            }
-                            worksheet.Cells[row, cell].Style.Numberformat.Format = "0."+points;
-                            worksheet.Cells[row, cell].Value = double.Parse(field);
-                        }
-                        //format Interger
-                        else if (Regex.Match(field, @"^\d+$").Success)
-                        {
-                            worksheet.Cells[row, cell].Style.Numberformat.Format = "0";
-                            worksheet.Cells[row, cell].Value = Int64.Parse(field);
-                        }
-                        //format Date
-                        else if (Regex.Match(field, @"^\d\d\/\d\d\/\d\d\d\d$").Success)
-                        {
-
-                            worksheet.Cells[row, cell].Style.Numberformat.Format = "dd-mm-yyyy";
-                            worksheet.Cells[row, cell].Value = DateTime.ParseExact(field, "dd/mm/yyyy",provider);
-                        }
-                        //its text
                         else
-                        {                            
-                            worksheet.Cells[row, cell].Value = field;
+                        {
+                            FormatAndInsertValue(provider, worksheet, row, cell, field, dateFormat);
                         }
-                                                       
-
+                        
                         cell++;
-
                     }
-                    
+
                     row++;
                 }
             }
+        }
 
-            worksheet.Cells.AutoFitColumns();
+        /// <summary>
+        /// Auto format the value and insert into field
+        /// 
+        /// </summary>
+        /// <param name="provider"></param>
+        /// <param name="worksheet"></param>
+        /// <param name="row"></param>
+        /// <param name="cell"></param>
+        /// <param name="field"></param>
+        private static void FormatAndInsertValue(CultureInfo provider, ExcelWorksheet worksheet, int row, int cell, string field,string dateFormat)
+        {
+            //format Currency
+            if (Regex.Match(field, @"^[0-9]+\.[0-9][0-9]$").Success)
+            {
+                worksheet.Cells[row, cell].Style.Numberformat.Format = "#,##0.00";
+                worksheet.Cells[row, cell].Value = double.Parse(field);
+            }
+            //format Currency Negative
+            else if (Regex.Match(field, @"^\([0-9]+\.[0-9][0-9]\)$").Success)
+            {
+                worksheet.Cells[row, cell].Style.Numberformat.Format = "#,##0.00";
+                worksheet.Cells[row, cell].Value = double.Parse(field.Replace("(", "-").Replace(")", ""));
+            }
+            //format Float Number
+            else if (Regex.Match(field, @"^[0-9]+\.[0-9][0-9][0-9]+$").Success)
+            {
+                string[] parts = field.Split('.');
+                string points = "";
+                for (int i = 0; i < parts[1].Length; i++)
+                {
+                    points += "0";
+                }
+                worksheet.Cells[row, cell].Style.Numberformat.Format = "#,##0." + points;
+                worksheet.Cells[row, cell].Value = double.Parse(field);
+            }
+            //format Float Number Negative
+            else if (Regex.Match(field, @"^\([0-9]+\.[0-9][0-9][0-9]+\)$").Success)
+            {
+                string[] parts = field.Split('.');
+                string points = "";
+                for (int i = 0; i < parts[1].Replace(")","").Length; i++)
+                {
+                    points += "0";
+                }
+                worksheet.Cells[row, cell].Style.Numberformat.Format = "#,##0." + points;
+                worksheet.Cells[row, cell].Value = double.Parse(field.Replace("(", "-").Replace(")", ""));
+            }
+            //format Interger
+            else if (Regex.Match(field, @"^[1-9]([0-9]+)?$").Success && field.Length < 16)
+            {
+                worksheet.Cells[row, cell].Style.Numberformat.Format = "#,##0";
+                worksheet.Cells[row, cell].Value = Int64.Parse(field);
+            }
+            //format Negative Interger
+            else if (Regex.Match(field, @"^\([1-9]([0-9]+)?\)$").Success && field.Length < 16)
+            {
+                worksheet.Cells[row, cell].Style.Numberformat.Format = "#,##0";
+                worksheet.Cells[row, cell].Value = Int64.Parse(field.Replace("(","-").Replace(")",""));
+            }
+            //format Date
+            else if (Regex.Match(field, @"^\d\d\/\d\d\/\d\d\d\d$").Success)
+            {
+
+                worksheet.Cells[row, cell].Style.Numberformat.Format = dateFormat;
+                worksheet.Cells[row, cell].Value = DateTime.ParseExact(field, "dd/mm/yyyy", provider);
+            }
+            //its text
+            else
+            {
+                worksheet.Cells[row, cell].Value = field;
+            }
+        }
+
+
+        // <summary>
+        /// Auto format the value and insert into field
+        /// 
+        /// </summary>
+        /// <param name="provider"></param>
+        /// <param name="worksheet"></param>
+        /// <param name="row"></param>
+        /// <param name="cell"></param>
+        /// <param name="field"></param>
+        private static void StyleCell(ExcelWorksheet worksheet, int row, int cell, string[] styles)
+        {
+            foreach (string style in styles)
+            {
+                string[] statement = style.Split('=');
+                string property = statement[0];
+                string value = statement[1];
+
+                switch (property)
+                {
+                    case "column-width":
+                        autofit = false;
+                        worksheet.Column(cell).Width = double.Parse(value);
+                        break;
+
+                    case "font-bold":
+                        worksheet.Cells[row, cell].Style.Font.Bold = true;
+                        break;
+
+                    case "font-italic":
+                        worksheet.Cells[row, cell].Style.Font.Italic = true;
+                        break;
+
+                    case "font-underline":
+                        worksheet.Cells[row, cell].Style.Font.UnderLine = true;
+                        break;
+
+                    case "font-color":
+                        worksheet.Cells[row, cell].Style.Font.UnderLine = true;
+                        break;
+
+                }
+            }
         }
     }
 }
--- CsvToXlsx/ReadMe.txt
+++ CsvToXlsx/ReadMe.txt
@@ -2,13 +2,51 @@
 
 Call the program from the CMD.
 
-Enter file path:Desired Sheet Name and lastly the file name
+Syntax:
+"File path > Desired Sheet Name > Date Format" output file name
 
-e.g
-================================================
+=====================================================================================================================================================================
+
 1 file
-CsvToXlsx.exe "C:\temp-copies\test.csv:sheet 1" "C:\temp-copies\combined.xlsx"
 
-===============================================
-multiple files
-CsvToXlsx.exe "C:\temp-copies\test.csv:sheet 1"  "C:\temp-copies\Bulk Delivery Note.csv:sheet 2" "C:\temp-copies\Orders.csv:Orders"  "C:\temp-copies\combined.xlsx"
+CsvToXlsx.exe "C:\temp-copies\test.csv>sheet 1>dd-mm-yyyy" "C:\temp-copies\combined.xlsx"
+
+=====================================================================================================================================================================
+
+Multiple files - Each on its own sheet
+
+CsvToXlsx.exe "C:\temp-copies\test.csv:sheet 1>dd-mm-yyyy"  "C:\temp-copies\Bulk Note.csv:sheet 2>dd-mm-yyyy" "C:\temp-copies\combined.xlsx"
+
+=====================================================================================================================================================================
+
+Auto Formating Of Cells
+
+0.00        => Formated to currency number
+(0.00)      => Formated to currency number negative
+
+0.00000     => Formated to decimal
+(0.00000)     => Formated to decimal negative
+
+100         => Formated to number
+(100)       => Formated to -100
+
+01/12/2019  => Formated to Date (enter desired format in CMD)
+
+*numbers starting with 0 or greater that 15 characters are processed as text
+
+=====================================================================================================================================================================
+
+Styling Cells
+
+Pass values with column data prepend with #$#property=value
+
+e.g Customer Details#$#font-weight=bold#$#text-color=#456879
+
+Available Styles:
+
+column-width=120  (if a width is specified for 1 column it should be specified for all. IF no columns specify a width, width will be set to auto.)
+
+font-bold=true
+font-italic=true
+font-underline=true
+font-color=#456FFF
